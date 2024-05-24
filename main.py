f0799dfb91963c1877a368eedd33ccb712abdd02
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2024-05-24 17:35:24 -0300

    Update of main.py to Version 3
    I changed some aspects of the project

--- main.py
+++ main.py
@@ -1,64 +1,127 @@
 import os
 
-#-PyPantry----------------------------------------------------------------------
-
-menu = ""
-while menu != "0":
+op_menu = ""
+while op_menu != "0":
   os.system("clear")
+  print()
   print("+----------------------+")
   print("|         MENU         |")
   print("|       DESPENSA       |")
   print("+----------------------+")
-  print("| 1 - Gerenciar        |")
-  print("| 2 - Relatórios       |")
+  print("| 1 - Produtos         |")
+  print("| 2 - Estoque          |") # codigo do produto e quantidade
   print("| 3 - Lista de Compras |")
-  print("| 4 - Informações      |")
+  print("| 4 - Relatórios       |")
+  print("| 5 - Informações      |")
   print("| 0 - Sair             |")
   print("+----------------------+")
-  # print("| Sinais de notificação existentes com base nos relatórios")
-  menu = input("| Escolha sua opção:")
+  # AQUI terão Sinais de notificação existentes com base nos relatórios
+  op_menu = input("| Escolha sua opção:")
 
-  if menu == "1":
-    os.system("clear")
-    opList = ""
-    while opList != "0":
+  if op_menu == "1":
+    os.system("Clear")
+    op_prod = ""
+    while op_prod != "0":
       os.system('clear')
+      print()
       print("+----------------------+")
-      print("|   GERENCIAR ITENS    |")
+      print("|       PRODUTOS       |")
       print("+----------------------+")
-      print("| 1 - Adicionar        |")
-      print("| 2 - Remover          |")
-      print("| 3 - Encontrar        |")
-      print("| 4 - Visualizar       |")
+      print("| 1 - Cadastrar        |") # Código do produto, nome, marca, categoria
+      print("| 2 - Exibir Dados     |")
+      print("| 3 - Compras          |") # data e hora (são a chave), cod, quantidade, valor
+      print("| 4 - Retiradas        |") # data e hora (são a chave), cod, quantidade
       print("| 0 - Voltar           |")
       print("+----------------------+")
-      opList = input("| Escolha sua opção:")
+      op_prod = input("| Escolha sua opção:")
 
-      if opList == "1":
+      if op_prod == "1":
         os.system("clear")
-        # Laço para ficar adicionando itens até inserir "Sair"
         print("+----------------------+")
-        print("|    ADICIONAR ITEM    |")
+        print("|  CADASTRAR PRODUTOS  |")
         print("+----------------------+")
-        nome = input("| Nome do item:")
-        categoria = input("| Categoria do item:")
-        quantidade = input("| Quantidade a ser adicionada:")
-        validade = input("| Validade do item:") # Apenas se o item tiver validade
-        data_compra = input("| Data de compra do item:") #Essa variável deve ter
-        #relação direta com a validade, pois pode haver o mesmo item comprado em
-        #datas diferentes (consequentemente com validades diferentes)
-        localizacao = input("| Localização do item:")
+        print("|")
+        nome = input("| Nome:")
+        print("|")
+        cod = input("| Código:")
+        print("|")
+        categoria = input("| Categoria:")
+        print("|")
+        marca = input("| Marca:")
+        print("Produto cadastrado com sucesso!")
+        input("<ENTER> para voltar")
+      
+      elif op_prod == "2":
+        os.system("clear")
+        print("+----------------------+")
+        print("|     EXIBIR DADOS     |")
+        print("+----------------------+")
+        print("|")
+        print("| Nome:", nome)
+        print("| Codigo:", cod)
+        print("| Categoria:", categoria)
+        print("| Marca:", marca)
         print()
-        print("Item adicionado com sucesso!")
+        input("<ENTER> para voltar")
+      
+      elif op_prod == "3":
         os.system("clear")
-        # Def de visualizar a despensa atual
+        print("+----------------------+")
+        print("|        COMPRA        |")
+        print("+----------------------+")
+        print("|")
+        cod = input("| Código do produto:") # Se o produto comprado não possui código existente, deve ser cadastrado antes
+        print("|")
+        dt_hr = input("| Data e Hora da Compra:")
+        print("|")
+        valor = input("| Valor da unidade:")
+        print("|")
+        quantidade = input("| Quantidade comprada:")
+        print("Compra cadastrada com sucesso!")
+        input("<ENTER> para voltar")
+      
+      elif op_prod == "4":
+        os.system("clear")
+        print("+----------------------+")
+        print("|       RETIRADA       |")
+        print("+----------------------+")
+        print("|")
+        cod = input("| Código do produto:") # É necessário um código existente para que aconteça o cadastro de retirada do produto
+        print("|")
+        dt_hr = input("| Data e Hora da Retirada:")
+        print("|")
+        quantidade = input("| Quantidade Retirada:") 
+        print("Retirada cadastrada com sucesso!")
         input("<ENTER> para voltar")
 
-  elif menu == "2":
+
+  if op_menu == "2":
+    os.system('clear')
+    op_est = ""
+    print()
+    print("+-------------------+")
+    print("|      ESTOQUE      |")
+    print("+-------------------+")
+    # cod e quantidade
+    op_est = input("| <ENTER> para voltar")
+
+  if op_menu == "3":
+    os.system('clear')
+    op_list = ""
+    print()
+    print("+----------------------------+")
+    print("|      LISTA DE COMPRAS      |")
+    print("+----------------------------+")
+    # Variável que recebe os itens que atingiram o estoque minimo
+    op_list = input("| <ENTER> para voltar")
+
+
+  if op_menu == "4":
     os.system("Clear")
-    opList = ""
-    while opList != "0":
+    op_rel = ""
+    while op_rel != "0":
       os.system('clear')
+      print()
       print("+----------------------------+")
       print("|    RELATÓRIOS DE ITENS     |")
       print("+----------------------------+")
@@ -69,24 +132,18 @@ while menu != "0":
       print("| 5 - Vencidos               |")
       print("| 0 - Voltar                 |")
       print("+----------------------------+")
-      opList = input("| Escolha sua opção:")
+      op_rel = input("| Escolha sua opção:")
 
-  elif menu == "3":
+  if op_menu == "5":
     os.system('clear')
-    opList = ""
-    print("+----------------------------+")
-    print("|      LISTA DE COMPRAS      |")
-    print("+----------------------------+")
-    # Variável que recebe os itens que atingiram o estoque minimo
-    opList = input("| <ENTER> para voltar")
-
-  elif menu == "4":
-    os.system('clear')
-    opList = ""
+    op_inf = ""
+    print()
     print("+-------------------------------------------+")
     print("|                INFORMAÇÕES                |")
     print("+-------------------------------------------+")
     print("| Projeto de Controle de Despensa Doméstica |")
     print("| Desenvolvedor: Guilherme Araújo / @0Haki  |")
     print("+-------------------------------------------+")
-    opList = input("| <ENTER> para voltar")
+    op_inf = input("| <ENTER> para voltar")
+
+print("Programa Encerrado!")
